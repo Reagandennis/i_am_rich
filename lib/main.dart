f5e2f7b449ebb4447e89744d88d1899deb0f87c1
Reagandennis <dennisowiti215@gmail.com>
2022-10-05 09:48:08 +0300

    app brewery

--- lib/main.dart
+++ lib/main.dart
@@ -4,8 +4,16 @@ void main() {
   runApp(
     MaterialApp(
       home: Scaffold(
+        backgroundColor: Colors.grey[800],
         appBar: AppBar(
-          title: Text('I am rich'),
+          title: const Text('I am rich'),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        body: const Center(
+          child: Image(
+            image: NetworkImage(
+                'https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885__480.jpg'),
+          ),
         ),
       ),
     ),
